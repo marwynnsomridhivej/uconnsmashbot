158e1dcb1679501790d071bd3d8a8a1f167b591a
marwynnsomridhivej <51054391+marwynnsomridhivej@users.noreply.github.com>
2020-09-02 17:48:31 -0400

    removed unused items and added an init

--- globalcommands.py
+++ globalcommands.py
@@ -20,6 +20,9 @@ default_env = ["YOUR_BOT_TOKEN",
 
 
 class GlobalCMDS:
+    
+    def __init__(self):
+        pass
 
     def init_env(self):
         if not os.path.exists('.env'):
@@ -33,56 +36,13 @@ class GlobalCMDS:
             return False
         return os.getenv(key)
 
-    def file_check(self, filenamepath: str, init):
-        if not os.path.exists(filenamepath):
-            with open(filenamepath, 'w') as f:
-                for string in init:
-                    f.write(string)
-
-    async def invkDelete(self, ctx):
-        if isinstance(ctx.channel, discord.TextChannel) and ctx.guild.me.guild_permissions.manage_messages:
-            await ctx.message.delete()
-
     async def msgDelete(self, message: discord.Message):
-        if isinstance(message.channel, discord.TextChannel) and message.guild.me.guild_permissions.manage_messages:
+        try:
             await message.delete()
-
-    async def timeout(self, ctx: commands.Context, title: str, timeout: int) -> discord.Message:
-        embed = discord.Embed(title=f"{title.title()} Timed Out",
-                              description=f"{ctx.author.mention}, your {title} timed out after {timeout} seconds"
-                              " due to inactivity",
-                              color=discord.Color.dark_red())
-        return await ctx.channel.send(embed=embed, delete_after=10)
-
-    async def cancelled(self, ctx: commands.Context, title: str) -> discord.Message:
-        embed = discord.Embed(title=f"{title.title()} Cancelled",
-                              description=f"{ctx.author.mention}, your {title} was cancelled",
-                              color=discord.Color.dark_red())
-        return await ctx.channel.send(embed=embed, delete_after=10)
-
-    async def panel_deleted(self, ctx: commands.Context, title: str) -> discord.Message:
-        embed = discord.Embed(title=f"{title.title()} Cancelled",
-                              description=f"{ctx.author.mention}, your {title} was cancelled because the panel was "
-                              "deleted or could not be found",
-                              color=discord.Color.dark_red())
-        return await ctx.channel.send(embed=embed, delete_after=10)
-
-    def isGuild(self, ctx):
-        if ctx.guild:
-            return True
-        else:
-            return False
+        except Exception:
+            pass
 
     def json_load(self, filenamepath: str, init: dict):
         if not os.path.exists(filenamepath):
             with open(filenamepath, 'w') as f:
                 json.dump(init, f, indent=4)
-
-    def prefix(self, ctx):
-        if not self.isGuild(self, ctx):
-            return "m!"
-
-        with open('prefixes.json', 'r') as f:
-            prefixes = json.load(f)
-
-        return prefixes[str(ctx.guild.id)]
