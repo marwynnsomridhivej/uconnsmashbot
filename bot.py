3dd64a88f7f8e65319e20cd1a6ce5440a9e8fdf2
marwynnsomridhivej <51054391+marwynnsomridhivej@users.noreply.github.com>
2020-09-07 12:10:38 -0400

    shortened version string so it actually fits on RP

--- bot.py
+++ bot.py
@@ -17,7 +17,7 @@ DISABLED_COGS = ["Blackjack", 'Coinflip', 'Connectfour', 'Oldmaid', 'Slots', 'Un
                  'Reactions', 'Moderation', 'Music', 'Utility']
 DISABLED_COMMANDS = []
 token_rx = re.compile(r'[MN]\w{23}.[\w-]{6}.[\w-]{27}')
-version = f"UCONN Smash Bot {gcmds.version}"
+version = f"USB {gcmds.version}"
 
 if os.path.exists('discord.log'):
     os.remove('discord.log')
